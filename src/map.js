786b923f316a8be6bbf7c36e564ed285749b08af
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-03-25 22:57:19 -0400

    Array and for loop with detailed breakdown

---
+++ src/map.js
@@ -0,0 +1,46 @@
+//Array of shopping list items
+let shoppingList = [
+  {
+    'item': 'banana',
+    'quantity': 6
+  },
+  {
+    'item': 'milk',
+    'quantity': 1
+  },
+  {
+    'item': 'yogurt',
+    'quantity': 4
+  },
+  {
+    'item': 'eggplant',
+    'quantity': 2
+  },
+  {
+    'item': 'bell pepper',
+    'quantity': 2
+  },
+  {
+    'item': 'sharp cheddar cheese',
+    'quantity': 1
+  }
+];
+
+//for loop using Array of shopping list items
+
+//First set a variable to create a new empty Array.
+//This allows us to hold on to the Array to use later.
+let shoppingList_names = [];
+
+//Now lets assemble our for loop.
+//A for loop takes three arguments in this order:
+//1. Assign a variable to the current position, in this case it says "start at 0"
+//2. Define the condition that must be true for the loop to continue, in this case 'i' must be less than or equal to the length of the Array
+  //This is done in the two central steps.
+  //'let max = shoppingList.length' holds on to the number of objects in the Array and assigns it the variable 'max'
+  //'i <= max' tells us 'i' must be less than or equal to 'max' (aka the number of objects in the Array)
+//3. What is going to occur every time we go throught the loop, in this case it says "add one to me"
+for (let i = 0, let max = shoppingList.length, i <=  max , i++){
+  //on the following line we will use the for loop to 'push' the information we want into our empty Array
+  shoppingList_names.push(shoppingList[i].name);
+}
